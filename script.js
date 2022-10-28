2c98d6a4963ece87f9ab10d1772ab4c14c6c618d
Max Christensen <max@Maxs-MacBook-Pro.local>
2022-10-28 13:45:07 +1300

    First Push

---
+++ script.js
@@ -0,0 +1,363 @@
+var movies = [
+    {
+        title:"Happy Gilmore",
+        poster: "src= img/poster1.jpeg",
+        genre: "Comedy",
+        blurb: "When Happy learns that his grandmother is going to lose her house, he decides to enter the golf tournament to win the prize money to save her home.",
+        starring: ["Adam Sandler"],
+        year: "1996",
+        rating: "M",
+        reviews: [
+            {
+                postedBy: "Andrew L.",
+                review: "A completely bizarre comedy mixed with a violent, unhinged, failed hockey player, played by Adam Sandler, who switches to golf to help his Grandma. His style is unorthodox but he's popular with the crowds and becomes a successful player. There's lots of jokes, treachery, love interest, nastiness and so many punches in many faces. I gave it 2 stars because I couldn't deal with Happy's initial violent behaviour, i.e. everything that goes wrong has to be met with a fusillade of punches or a foul lmouthed hissy fit."
+            },
+            {
+                postedBy: "Kaelin Gonzales",
+                review: "I love this movie!! One of Adam Sandler’s best, along with Billy Madison, and The Waterboy!! Happy Gilmore is still one of the best movies that I like! I never get board of watching this movie! It is too good, and funny! My favorite part is when Happy Gilmore says, go to a happy place! Best clip ever! I want to say that I do enjoy this movie! It has funny characters, and a great storyline! I do recommend this to anybody. Definitely worth a watch!! I give it a full 5/5 stars!"
+            },],
+        trailerURL: "https://www.imdb.com/video/vi3306209817/?playlistId=tt0116483&ref_=tt_pr_ov_vi"
+    },
+
+    {
+        title:"Blade Runner 2049",
+        poster: "src= img/poster2.jpg",
+        genre: "Sci-Fi",
+        blurb: "Young Blade Runner K's discovery of a long-buried secret leads him to track down former Blade Runner Rick Deckard, who's been missing for thirty years.",
+        starring: ["Harrison Ford"],
+        year: "2017",
+        rating: "R",
+        reviews: [
+            {
+                postedBy: "Abood Ash",
+                review: "I had always wanted to see the sequel to the original Blade Runner for a long time. It had been on my watchlist for a while and by the time I watched it, it did not disappoint. If Blade Runner is highly considered to be influencial to the sci-fi genre, Blade Runner 2049 builds on the fundation of the first film and proves to be a filmmaking achievement in itself."
+            },
+            {
+                postedBy: "Matthew Brady",
+                review: "Wow! WOW WOW WOW WOW WOW WOW! And another WOW!"
+            },],
+        trailerURL: "https://www.imdb.com/video/vi3362371865/?playlistId=tt1856101&ref_=tt_pr_ov_vi"
+    },
+
+    {
+        title:"The Dark Knight",
+        poster: "src= img/poster3.jpg",
+        genre: "Action",
+        blurb: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+        starring: ["Christian Bale"],
+        year: "2008",
+        rating: "PG-13",
+        reviews: [
+            {
+                postedBy: "Walter Hines",
+                review: "In the Trilogy of Dark knight by Christopher Nolan,Shows us the real world approach on the comic created character Batman. Chris Actually showed the Batman in a different perspective that was new to the comic fans, the 3 movies shows the journey of batman how he evolves in every story.the three movies reflect the Beginning, fall and rise of batman So The Dark Knight (2008) is about the fall of batman."
+            },
+            {
+                postedBy: "Shubham Dutta",
+                review: "Just when you thought that you had overdosed on superheroes, the Caped Crusader returns in one of the most stylish extravaganzas in years. And yes, he blows your socks off."
+            },],
+        trailerURL: "https://www.imdb.com/video/vi324468761/?playlistId=tt0468569&ref_=tt_ov_vi"
+    },
+
+    {
+        title:"Bullet Train",
+        poster: "src= img/poster4.jpg",
+        genre: "Action",
+        blurb: "Five assassins aboard a swiftly-moving bullet train find out that their missions have something in common.",
+        starring: ["Brad Pitt"],
+        year: "2022",
+        rating: "R",
+        reviews: [{}, {}, {}],
+        trailerURL: "https://www.imdb.com/video/vi1743438361/?playlistId=tt12593682&ref_=tt_ov_vi"
+    },
+
+    {
+        title:"Chronicle",
+        poster: "src= img/poster5.jpg",
+        genre: "Sci-Fi",
+        blurb: "Three high school friends gain superpowers after making an incredible discovery underground. Soon they find their lives spinning out of control and their bond tested as they embrace their darker sides.",
+        starring: ["Dane DeHaan"],
+        year: "2012",
+        rating: "PG-13",
+        reviews: [{}, {}, {}],
+        trailerURL: "https://www.imdb.com/video/vi2464325145/?playlistId=tt1706593&ref_=tt_ov_vi"
+    },
+
+    {
+        title:"Cars",
+        poster: "src= img/poster6.jpeg",
+        genre: "Animation",
+        blurb: "On the way to the biggest race of his life, a hotshot rookie race car gets stranded in a rundown town, and learns that winning isn't everything in life.",
+        starring: ["Owen Wilson"],
+        year: "2006",
+        rating: "G",
+        reviews: [{}, {}, {}],
+        trailerURL: "https://www.imdb.com/video/vi2937628953/?ref_=ttvi_vi_imdb_7"
+    },
+
+    {
+        title:"Rush",
+        poster: "src= img/poster7.jpg",
+        genre: "Action",
+        blurb: "The merciless 1970s rivalry between Formula One rivals James Hunt and Niki Lauda.",
+        starring: ["Chris Hemsworth"],
+        year: "2013",
+        rating: "R",
+        reviews: [{}, {}, {}],
+        trailerURL: "https://www.imdb.com/video/vi1998301465/?playlistId=tt1979320&ref_=tt_ov_vi"
+    },
+
+    {
+        title:"Goodfellas",
+        poster: "src= img/poster8.jpg",
+        genre: "Crime",
+        blurb: "The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners Jimmy Conway and Tommy DeVito in the Italian-American crime syndicate.",
+        starring: ["Robert De Niro"],
+        year: "2013",
+        rating: "R",
+        reviews: [{}, {}, {}],
+        trailerURL: "https://www.imdb.com/video/vi2673654553/?playlistId=tt0099685&ref_=tt_ov_vi"
+    },
+
+    {
+        title:"The Cabin in the Woods",
+        poster: "src= img/poster9.jpg",
+        genre: "Horror",
+        blurb: "Five friends go for a break at a remote cabin, where they get more than they bargained for, discovering the truth behind the cabin in the woods.",
+        starring: ["Chris Hemsworth"],
+        year: "2011",
+        rating: "R",
+        reviews: [{}, {}, {}],
+        trailerURL: "https://www.imdb.com/video/vi2572394777/?playlistId=tt1259521&ref_=tt_ov_vi"
+    },
+
+    {
+        title:"Knives Out",
+        poster: "src= img/poster10.jpg",
+        genre: "Crime",
+        blurb: "A detective investigates the death of the patriarch of an eccentric, combative family.",
+        starring: ["Daniel Craig"],
+        year: "2019",
+        rating: "PG-13",
+        reviews: [{}, {}, {}],
+        trailerURL: "https://www.imdb.com/video/vi2464857881/?playlistId=tt8946378&ref_=tt_ov_vi"
+    },    
+]
+
+
+// Get all filter inputs
+var genre = document.getElementById('genreSelect');
+var starring = document.getElementById('starringSelect');
+var rating = document.getElementById('ratingSelect');
+var search = document.getElementById('searchBtn');
+var searchInput = document.getElementById('searchInput');
+
+
+genre.addEventListener('change', filterGenre);
+starring.addEventListener('change', filterStarring);
+rating.addEventListener('change', filterRating);
+// straight compare search
+search.addEventListener('click', searchMovies);
+// on KeyUp search search and compare check using include()
+searchInput.addEventListener('keyup', function(event) {
+    var input = event.target.value;
+    var moviesOutput = document.getElementById('movieOutput');
+    moviesOutput.innerHTML = " ";
+    for(i = 0; i < movies.length; i++){
+        if(movies[i].title.toLowerCase().includes(input.toLowerCase())){
+            var movie = movies[i];
+            moviesOutput.innerHTML +=  
+            `<div class="col-4 p-2">
+                    <div class="card">
+                        <img ${movie.poster} class="card-img-top" alt="Movie Poster">
+                        <div class="card-body">
+                        <h5 class="card-title">${movie.title}</h5>
+                        <h6 class="card-text">${movie.year}</h6>
+                        <p class="card-text">${movie.rating}</p>
+                        <a href="#" class="btn">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            `
+        }
+    }
+})
+
+searchStarring.addEventListener('keyup', function(event) {
+    var input = event.target.value;
+    var moviesOutput = document.getElementById('movieOutput');
+    moviesOutput.innerHTML = " ";
+    for(var i = 0;i < movies.length; i++) {
+        for(j = 0; j < movies[i].starring.length; j++) {
+            var movie = movies[i];
+            var stars = movies[i].starring[j];
+            if(stars.toLowerCase().includes(input.toLowerCase())) {
+            moviesOutput.innerHTML += 
+                `
+                <div class="col-4 p-2">
+                        <div class="card">
+                            <img ${movie.poster} class="card-img-top" alt="Movie Poster">
+                            <div class="card-body">
+                                <h5 class="card-title">${movie.title}</h5>
+                                <h6 class="card-text">${movie.year}</h6>
+                                <p class="card-text">${movie.rating}</p>
+                                <a href="#" class="btn">Read More</a>
+                            </div>
+                        </div>
+                </div>
+            `
+            }
+        }
+    }
+})
+
+
+function filterGenre() {
+    var moviesOutput = document.getElementById('movieOutput');
+    moviesOutput.innerHTML = " ";
+    for(var i = 0; i < movies.length; i++) {
+        var movie = movies[i];
+        if(genre.value === movie.genre) {
+            moviesOutput.innerHTML +=  
+            `<div class="col-4 p-2">
+                    <div class="card">
+                        <img ${movie.poster} class="card-img-top" alt="Movie Poster">
+                        <div class="card-body">
+                        <h5 class="card-title">${movie.title}</h5>
+                        <h6 class="card-text">${movie.year}</h6>
+                        <p class="card-text">${movie.rating}</p>
+                        <a href="#" class="btn">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            `
+        }
+    }
+}
+
+filterGenre();
+
+
+function filterStarring() {
+    var moviesOutput = document.getElementById('movieOutput');
+    moviesOutput.innerHTML = " ";
+    for(var i = 0;i < movies.length; i++) {
+        for(j = 0; j < movies[i].starring.length; j++) {
+            var movie = movies[i];
+            var stars = movies[i].starring[j];
+            if(starring.value === stars) {
+            moviesOutput.innerHTML += 
+                `
+                <div class="col-4 p-2">
+                        <div class="card">
+                            <img ${movie.poster} class="card-img-top" alt="Movie Poster">
+                            <div class="card-body">
+                                <h5 class="card-title">${movie.title}</h5>
+                                <h6 class="card-text">${movie.year}</h6>
+                                <p class="card-text">${movie.rating}</p>
+                                <a href="#" class="btn">Read More</a>
+                            </div>
+                        </div>
+                </div>
+            `
+            }
+        }
+    }
+}
+
+filterStarring();
+
+
+function filterRating() {
+    var moviesOutput = document.getElementById('movieOutput');
+    moviesOutput.innerHTML = " ";
+    for(var i = 0; i < movies.length; i++) {
+        var movie = movies[i];
+        if(rating.value === movie.rating) {
+            moviesOutput.innerHTML +=  
+            `<div class="col-4 p-2">
+                    <div class="card">
+                        <img ${movie.poster} class="card-img-top" alt="Movie Poster">
+                        <div class="card-body">
+                        <h5 class="card-title">${movie.title}</h5>
+                        <h6 class="card-text">${movie.year}</h6>
+                        <p class="card-text">${movie.rating}</p>
+                        <a href="#" class="btn">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            `
+        }
+    }
+}
+
+filterRating();
+
+function getAllMovies() {
+    var moviesOutput = document.getElementById('movieOutput');
+    for(var i = 0; i < movies.length; i++) {
+        var movie = movies[i];
+        moviesOutput.innerHTML += 
+            `<div class="col-12 col-md-4 mb-5">
+                    <div class="card">
+                        <img ${movie.poster} class="card-img-top" alt="${movie.title} Poster">
+                        <div class="card-body">
+                        <h5 class="card-title">${movie.title}</h5>
+                        <h6 class="card-text">${movie.year}</h6>
+                        <p class="card-text">${movie.rating}</p>
+                        <a href="#" class="btn" value="${i}" data-bs-toggle="modal" data-bs-target="#movieModal">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            `
+    }
+}
+
+getAllMovies();
+
+function searchMovies(){
+    var moviesOutput = document.getElementById('movieOutput');
+    moviesOutput.innerHTML = " ";
+    var searchValue = searchInput.value;
+    for(i=0; i < movies.length; i++) {
+        var movie = movies[i];
+        if(searchValue.toLowerCase() == movie.title.toLowerCase()) {
+            moviesOutput.innerHTML += 
+            `<div class="col-4 p-2">
+                    <div class="card">
+                        <img ${movie.poster} class="card-img-top" alt="Movie Poster">
+                        <div class="card-body">
+                        <h5 class="card-title">${movie.title}</h5>
+                        <h6 class="card-text">${movie.year}</h6>
+                        <p class="card-text">${movie.rating}</p>
+                        <a href="#" class="btn">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            `
+        }
+    }
+}
+
+
+
+
+
+// function getAllMovieStars() {
+//     var starringOutput = document.getElementById('starringSelect');
+//     starringOutput.innerHTML =
+//     `<option selected>Starring</option>`
+//     var stars = [];
+//     for(var i = 0; i < movies.length; i++) {
+//         var movie = movies[i];
+//         var starring = movie.starring;
+//         for( j = 0; j < starring.length; j++){
+//             stars.push(starring[j])
+//         }
+//     }
+//     for(a = 0; a < stars.length; a++){
+//         starringOutput.innerHTML +=
+//             `<option value="${stars[a]}">${stars[a]}</option>`
+//     }
+// }
+// getAllMovieStars();
